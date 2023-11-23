3e85e1c72af0a80193a955d8dd4213e828dc8a46
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-11-23 23:44:29 +0300

    Javascript
    javascript exercises storage kullanımı

---
+++ Javascript/Exercises/exercise1/script.js
@@ -0,0 +1,27 @@
+
+//let counter = 0
+let counter= localStorage.getItem("counter")
+let counterDOM = document.getElementById("counter")
+let increase = document.getElementById("increase")
+let decrease = document.getElementById("decrease")
+let clear = document.getElementById("clear")
+
+counterDOM.innerHTML = counter
+
+increase.addEventListener("click", clickEvent)
+decrease.addEventListener("click", clickEvent)
+clear.addEventListener("click", clickClear)
+
+
+function clickEvent() {
+    console.log(typeof(counter));
+    console.log(this.id);
+    this.id == "increase" ?counter += 1 :counter -= 1
+    localStorage.setItem("counter", counter)
+    counterDOM.innerHTML = counter
+}
+
+function clickClear() {
+    localStorage.clear()
+    counterDOM.innerHTML = "0"
+}
